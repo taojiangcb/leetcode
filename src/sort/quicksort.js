03ec24d310a6f0c404bffa01f494a4b840176409
wb-jt706470 <wb-jt706470@alibaba-inc.com>
2021-01-08 09:26:55 +0800

    quick sort

--- src/sort/quicksort.js
+++ src/sort/quicksort.js
@@ -5,20 +5,21 @@
  * @param {Function} fun          // 比对大小 默认 小的放左边区间 大的放右边区间
  */
 const quickSort = function (nums, fun = (a, b) => (a < b)) {
-  
-  const len = nums.length;
-  if (len <= 1) return nums;
-  //取中间值
-  const pivot = nums[len >> 1];
-  //左右区间
-  const l = [], r = [];   
 
+  const len = nums.length;
+  // 4. 直到长度为 1 结束
+  if (len <= 1) return nums;
+
+  // 1. 取中间值
+  const pivot = nums[len >> 1];
+  // 2. 左右区间
+  const l = [], r = [];   
   for (let i = 0; i < len; i++) {
     if (nums[i] === pivot) continue;
     fun(nums[i], pivot)       //比对大小
       ? l.push(nums[i])       //左边区间
       : r.push(nums[i]);      //右边区间
   }
-  // 递归拆分细化
+  // 3. 递归拆分 [左,中,右]
   return quickSort(l, fun).concat([pivot], quickSort(r, fun));
 }
